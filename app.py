aa88f2873a0f17ef77bc8d66f5dfc488866a79fe
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-12 19:32:25 +0000

    installed pymongo and dns python

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectId import ObjectId
+import os
 
 app = Flask(__name__)
+
+if __name__=='__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)s
