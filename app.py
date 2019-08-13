2aef14bcfe6dea0ff6cd73c4b5e951322474b5a5
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-13 17:34:30 +0000

    Continued app set up, tested mongo db connected, updated manual testing doc with this test function

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
-from flask_pymongo import PyMongo
-from bson.objectId import ObjectId
+import pymongo
+from bson.objectid import ObjectId
 import os
 
 app = Flask(__name__)
 
+MONGODB_URI = os.getenv('MONGO_URI', 'mongodb://localhost')
+DBS_NAME = "bakingBookRecipes"
+COLLECTION_NAME = 'bakingBookRecipes'
+
+
+
+
 if __name__=='__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)s
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+
+
+    
