1d095feb0c94c82e77a9c1afc797232cfba6741a
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-17 13:01:57 +0000

    removed url_for for css

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
-
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'bakingBookRecipes'
