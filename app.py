b51c764b9c6e4cc94d4960c9f47dfea16e7b9152
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-19 20:46:22 +0000

    added recipes mongo back into recipe card template render, testing if all recipes still displayed

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def show_recipes():
     
 @app.route('/recipe_card')
 def recipe_card():
-    return render_template("recipecard.html")
+    return render_template("recipecard.html", recipes=mongo.db.recipes.find())
 
 
 if __name__=='__main__':
