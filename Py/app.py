439a4dd700756a80092ee83302045f8df31279b8
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-24 11:05:49 +0000

    Added Py folder and moved python files into it, adjusted import

--- Py/app.py
+++ Py/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from BakeBook.forms.py import AddRecipeForm
+from Py.forms import AddRecipeForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
