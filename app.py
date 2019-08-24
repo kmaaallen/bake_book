7f1fd01186d5fed354791ec86765a02448ecb1ae
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-24 10:49:59 +0000

    imported form from forms.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_wtf import Form
+from forms.py import AddRecipeForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
