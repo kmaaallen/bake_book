8033b727365b6d8c7cd573820fd6e86539c46997
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 17:04:14 +0000

    removed encoding

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def login():
         login_username = users.find_one({'user' : request.form['username']})
     
         if login_username:
-            if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_username['password']): 
+            if bcrypt.checkpw(request.form['password'], login_username['password']): 
            # new_pw = bcrypt.hashpw(request.form['password'].encode('utf-8'))
             #db_pw = login_username['password'].decode()
            # if new_pw.decode() == login_username['password']:
@@ -47,7 +47,7 @@ def sign_up():
         
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'name': request.form['username'], 'password' : hashpass})
+            users.insert_one({'name': request.form['username'], 'password' : hashpass.decode()})
             session['username'] = request.form['username']
             return redirect (url_for('show_recipes'))
         
