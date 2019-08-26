9362c316a493751d67851c1051c27d073a9f4921
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 17:56:24 +0000

    testing is username is not found

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def login():
     if request.method == 'POST':
         users = mongo.db.users
         login_username = users.find_one({'user' : request.form['username']})
-        if login_username is not None:
+        if login_username is None:
             return 'login_username is not none'
             #if bcrypt.hashpw(request.form['password'].encode('utf-8')).decode() == login_username['password']: 
               #  session['username'] = request.form['username']
