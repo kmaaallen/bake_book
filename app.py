dcbf0893c6574cdd5a3c2d1c54c72486d6f038b4
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 17:14:53 +0000

    stored hashed input as new pw variable

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def login():
         login_username = users.find_one({'user' : request.form['username']})
     
         if login_username:
-            if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_username['password']): 
+            new_pw = bcrypt.hashpw(request.form['password'].encode('utf-8')).decode();
+            if bcrypt.checkpw(new_pw, login_username['password']): 
            # new_pw = bcrypt.hashpw(request.form['password'].encode('utf-8'))
             #db_pw = login_username['password'].decode()
            # if new_pw.decode() == login_username['password']:
