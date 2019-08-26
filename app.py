1916cc57483f6f321a70db15ee65320babc3971b
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 16:36:06 +0000

    added missing encode

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def login():
         login_username = users.find_one({'user' : request.form['username']})
     
         if login_username:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_username['password'].('utf-8')) == login_username['password'].encode('utf-8'):
-                print(bcrypt.hashpw(request.form['password'].encode('utf-8'), login_username['password'].('utf-8')) + login_username['password'].encode('utf-8'))
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_username['password'].encode('utf-8')) == login_username['password'].encode('utf-8'):
+                print(bcrypt.hashpw(request.form['password'].encode('utf-8'), login_username['password'].encode('utf-8')) + login_username['password'].encode('utf-8'))
            # new_pw = bcrypt.hashpw(request.form['password'].encode('utf-8'))
             #db_pw = login_username['password'].decode()
            # if new_pw.decode() == login_username['password']:
