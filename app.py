63397492f2dcc36066fd9ed1101bcb676f30933a
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 13:28:36 +0000

    updated sign_up function to check for existing user and add new one

--- app.py
+++ app.py
@@ -27,6 +27,18 @@ def login():
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     form= SignupForm(request.form)
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'user' : request.form['username']})
+        
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert_one({'name': request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect (url_for('show_recipes'))
+        
+        return 'That username already exists, please choose another.'
+            
     return render_template('signup.html', form = form)
     
 @app.route('/recipe_card/<recipe_id>')
