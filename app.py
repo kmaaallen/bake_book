ff0a8fde06778f74b99adbdc0741a62affa1aa88
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 11:49:13 +0000

    added ObjectId syntax

--- app.py
+++ app.py
@@ -49,8 +49,7 @@ def submit_recipe():
                "created_by": "current user"
            }
            )
-    new_recipe_id = new_recipe.inserted_id
-    return redirect(url_for('recipe_card', recipe_id=new_recipe_id))
+    return redirect(url_for('recipe_card', recipe_id=ObjectId(new_recipe.inserted_id)))
     
 if __name__=='__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
