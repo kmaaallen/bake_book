377005ee8e832b7b9d115d805d84d3711fa07323
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 16:51:12 +0000

    added print statements to debug

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def login():
         login_username = users.find_one({'user' : request.form['username']})
     
         if login_username:
+            print(request.form['password'].encode('utf-8') + login_username['password'])
             if bcrypt.checkpw(request.form['password'].encode('utf-8'), login_username['password']): 
            # new_pw = bcrypt.hashpw(request.form['password'].encode('utf-8'))
             #db_pw = login_username['password'].decode()
