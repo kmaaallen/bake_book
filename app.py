9caa53106d9e02afb5de1bc697d77cdeba4a1b09
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 12:02:44 +0000

    added get method in again

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def save_recipe(recipe_id, user_id):
     user.update_one({ '$push': { 'saved_recipes': ObjectId(recipe_id) }})
     return render_template("recipecard.html", recipes=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
 
-@app.route('/submit_recipe', methods=['POST'])
+@app.route('/submit_recipe', methods=['GET','POST'])
 def submit_recipe():
     new_recipe = None
     form = AddRecipeForm(request.form)
