346f628c5e3c943fe4a0b82795350f46f8bc0b04
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-08-26 13:02:32 +0000

    imported form classes from form.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from forms.forms import AddRecipeForm
+from forms.forms import AddRecipeForm, LoginForm, SignupForm
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
