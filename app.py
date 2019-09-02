515318ccab550fdbad7dd97701e6d398a211165d
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-02 17:58:23 +0000

    updated recipe to recipe to update to avoid confusion in variable names

--- app.py
+++ app.py
@@ -109,12 +109,12 @@ def my_recipes():
 
 @app.route('/edit_recipe/<recipe_id>', methods=['GET','POST'])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    recipe_to_update = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     form = AddRecipeForm()
     if request.method == 'POST':
        # form = AddRecipeForm(data=recipe)
         form.populate_obj(recipe)
-    return render_template("editrecipe.html", form=form, recipe=recipe)
+    return render_template("editrecipe.html", form=form, recipe=recipe_to_update)
     
 @app.route('/update_recipe/<recipe_id>')
 def update_recipe(recipe_id):
