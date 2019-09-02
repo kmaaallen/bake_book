91b5cd0e451c85c1a6ff9c9c0bcb43237becab0a
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-02 17:23:56 +0000

    removed request from edit_recipe arguments

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def my_recipes():
     return render_template("myrecipes.html", recipes=mongo.db.recipes.find({'created_by': username}))
 
 @app.route('/edit_recipe/<recipe_id>', methods=['GET','POST'])
-def edit_recipe(request, recipe_id):
+def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     form = AddRecipeForm(request.POST, recipe)
     if request.method == 'POST':
