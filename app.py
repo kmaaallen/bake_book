22eb95e483097f597e404cbaa8863b358203eb14
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-03 17:10:07 +0000

    updated edit_recipe function

--- app.py
+++ app.py
@@ -109,9 +109,13 @@ def my_recipes():
 
 @app.route('/edit_recipe/<recipe_id>', methods=['GET','POST'])
 def edit_recipe(recipe_id):
-    recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
-    form = AddRecipeForm(request.form, obj=recipe)
-    return render_template("editrecipe.html", form=form, recipe=recipe)
+    if request.method == 'POST':
+        form = AddRecipeForm(request.form, obj=recipe)
+        recipe = request.form
+        return render_template("editrecipe.html", recipe = recipe)
+    #recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
+    #form = AddRecipeForm(request.form, obj=recipe)
+    #return render_template("editrecipe.html", form=form, recipe=recipe)
     
 @app.route('/update_recipe/<recipe_id>')
 def update_recipe(recipe_id):
