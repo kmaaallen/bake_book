e38c05c185cda4a9ccc855ac2ce588cbc18871cf
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-03 16:57:03 +0000

    defined reciped as find one recipe record

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def my_recipes():
 def edit_recipe(recipe_id):
     recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})
     form = AddRecipeForm(obj=recipe)
-    return render_template("editrecipe.html", form=form, recipe = recipe)
+    return render_template("editrecipe.html", form=form, recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
     
 @app.route('/update_recipe/<recipe_id>')
 def update_recipe(recipe_id):
