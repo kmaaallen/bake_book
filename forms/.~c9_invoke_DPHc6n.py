605ba3dfe3ef8b16f6d77845a17008c5becaa6c4
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-04 21:39:08 +0000

    removed join statement

---
+++ forms/.~c9_invoke_DPHc6n.py
@@ -0,0 +1,22 @@
+from flask_wtf import Form
+from wtforms import TextField, TextAreaField, StringField, SubmitField, PasswordField, FileField
+from wtforms.validators import DataRequired
+
+class AddRecipeForm(Form):
+    recipe_title = StringField("Recipe Title", validators=[DataRequired()])
+    sub_title = TextField("Recipe Sub-title", validators=[DataRequired()])
+    makes = TextField("Makes")
+    takes = TextField("Takes")
+    ingredients = TextField("Ingredients:")
+    method = TextField("Method:")
+    tags = TextField("Tags:")
+    submit = SubmitField("Send")
+    
+class LoginForm(Form):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    
+class SignupForm(Form):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+ 
