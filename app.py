12e14c2abff7e0987513fe65065565bae4108ec5
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-05 17:27:55 +0000

    removed flash comment - redirecting properly so flash message no longer needed

--- app.py
+++ app.py
@@ -154,7 +154,6 @@ def edit_recipe(recipe_id):
             })
 
          # "recipe_img_name": recipe_img_name
-        #flash('Recipe has been successfully updated')
         return render_template('recipecard.html', recipes=mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)}))
     return render_template('editrecipe.html', recipe=recipe, form=form)
 
