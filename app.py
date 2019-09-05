c59b87f89f9532ee46f5e8924514703598b8b5f7
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-05 18:16:15 +0000

    added user record line to save recipe function to update array in user record

--- app.py
+++ app.py
@@ -83,12 +83,9 @@ def recipe_card(recipe_id):
 
 @app.route('/save_recipe/<recipe_id>', methods=['POST'])
 def save_recipe(recipe_id):
-    print(recipe_id)
     username = session['username']
-    print('user is: ' + username)
     user_record = mongo.db.users.find_one({'username' : username})
-    print("user record user is " + user_record.username)
-    #mongo.db.users.update('username' : session['username'], {$push: {'saved_recipes' : '1' })
+    user_record.update('username' : session['username'], {$push: {'saved_recipes' : '1' })
 
 
 @app.route('/submit_recipe', methods=['GET', 'POST'])
