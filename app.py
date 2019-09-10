a31ec54bb5967de9dcca8563eacbf2e84a76cc5f
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-10 18:16:45 +0000

    added index on db not in code

--- app.py
+++ app.py
@@ -194,7 +194,6 @@ def search_results():
     if request.method == 'POST':
         keywords = request.form.get("keywords")
         flash(keywords)
-        mongo.db.recipes.createIndex( { "recipe_title": "text" } )
         recipes=mongo.db.recipes.find({"$text": { "$search": keywords}})
     return render_template('recipes.html', recipes=recipes )
 
