5e72639525ef221e03fb521a65f0c7d3be2aa878
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-10 17:29:50 +0000

    adding search functionality and linking to keywords in search bar

--- app.py
+++ app.py
@@ -188,6 +188,10 @@ def delete_recipe(recipe_id):
     recipes = mongo.db.recipes
     recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('my_recipes'))
+    
+@app.route('/search_results/<keywords>')
+def search_results(keywords):
+    mongo.db.recipes.find({$text: { $search: keywords}})
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
