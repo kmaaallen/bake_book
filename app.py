ec6259efc27adafbf4437f8d553f4be3c4875355
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-13 17:19:43 +0000

    fixing indentation errors in image function

--- app.py
+++ app.py
@@ -127,20 +127,14 @@ def submit_recipe():
         file_type = request.args.get('file_type')
         s3 = boto3.client('s3')
     
-        presigned_post = s3.generate_presigned_post(
-        Bucket = S3_BUCKET,
-        Key = file_name,
-        Fields = {"acl": "public-read", "Content-Type": file_type},
-        Conditions = [
-         {"acl": "public-read"},
-        {"Content-Type": file_type}
-     ],
-        ExpiresIn = 3600
-    )
-return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })
+       presigned_post = s3.generate_presigned_post(Bucket=S3_BUCKET,
+        Key=file_name, Fields={'acl': 'public-read',
+        'Content-Type': file_type}, Conditions=[{'acl': 'public-read'},
+        {'Content-Type': file_type}], ExpiresIn=3600)
+
+return json.dumps({'data': presigned_post,
+                  'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET,
+                  file_name)})
 
         if request.method == 'POST':
             new_recipe = recipes.insert_one({
