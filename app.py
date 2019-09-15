7635edeacdb0d677934be62847be09d94f44dfc7
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-09-15 19:31:32 +0000

    fixed extensions filename

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in EXTENSIONS
 
 mongo = PyMongo(app)
 
