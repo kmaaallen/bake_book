94e2497a5723b750f415fd7e275844111dbfcce6
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-10-04 19:27:42 +0000

    updated submit recipe with check for image content function

--- app.py
+++ app.py
@@ -137,14 +137,40 @@ def submit_recipe():
         recipes = mongo.db.recipes
         form_normal = request.form.to_dict()
         flat_form = request.form.to_dict(flat=False)
-        
         if request.method == 'POST':
-            default_img_url = 'https://i.imgur.com/dKEvSLE.png'
-            input_img_url = request.form['recipe_url']
-            if input_img_url != '' and input_img_url.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-                recipe_url = input_img_url
-            else:
-                recipe_url = default_img_url
+            default_img_url = '/static/images/default.png'
+            try:
+            #tests image first
+                test_image = request.form['recipe_url']
+                    if url_parse(test_image).scheme:
+                    #data urls are excluded
+                        if url_parse(test_image).scheme == 'data':
+                            recipe_image = default_image_url
+                            else:
+                            #prevents 403 error
+                                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+                                url_request = urllib.request.Request(test_image, headers = headers)
+                                test = urllib.request.urlopen(url_request)
+                                #gets url type
+                                url_type = test.info()['Content-type']
+                                    if url_type.endswith("png") or url_type.endswith("jpeg") or url_type.endswith("gif"):
+                                        recipe_image = request.form["recipe_url"]
+                                        else:
+                                            recipe_image = default_image_url
+                                            else:
+                                            #if user created a faulty url, default image is used
+                                                recipe_image = default_image
+           except Exception as e:
+               #inform them that a general error has occurred
+               pass
+               recipe_image = default_image_url
+            
+            
+            # #input_img_url = request.form['recipe_url']
+            # if input_img_url != '' and input_img_url.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            #     recipe_url = input_img_url
+            # else:
+            #     recipe_url = default_img_url
             new_recipe = recipes.insert_one({
                 'recipe_title': form_normal['recipe_title'],
                 'sub_title': form_normal['sub_title'],
@@ -153,7 +179,7 @@ def submit_recipe():
                 'ingredients': flat_form['ingredients'],
                 'method': flat_form['method'],
                 'created_by': session['username'],
-                'recipe_url': recipe_url
+                'recipe_url': recipe_image
                 })
             return redirect(url_for('recipe_card',
                             recipe_id=new_recipe.inserted_id))
