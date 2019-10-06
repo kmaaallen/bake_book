25a6a63674bd1f2733a4b489e9ba2617d780f299
Ubuntu <ubuntu@ip-172-31-40-87.ec2.internal>
2019-10-06 07:51:38 +0000

    testing temote connection

--- app.py
+++ app.py
@@ -247,3 +247,6 @@ def search_results():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
             )), debug=True)
+
+
+#testing remote connection
