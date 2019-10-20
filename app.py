035c70562896ccca5f08c99dcf13dfe2ffa0b20f
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-10-20 20:57:24 +0000

    disabled debug mode

--- app.py
+++ app.py
@@ -223,4 +223,4 @@ def search_results():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT'
-            )), debug=True)
+            )), debug=False)
