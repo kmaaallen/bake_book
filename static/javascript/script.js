38f75c3a4313abd78664f39c0e394ec2896a5aa8
Ubuntu <ubuntu@ip-172-31-35-192.ec2.internal>
2019-10-24 21:35:43 +0000

    validated js code

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,16 +1,16 @@
  //functions to add and remove extra inputs for method and ingredients
  function removeInput(e) {
-     e.parentElement.remove(e.parentElement);
+  e.parentElement.remove(e.parentElement);
  }
 
  function addIngredient() {
-     var newInput = $("<div><input required type='text' value='' class='ingredients extra-input extra-ing-input' name='ingredients'></input><a class='btn add-btn' onClick='removeInput(this)'><i class='material-icons del-btn-ing'>delete</i></a><a class='btn add-btn' onClick='addIngredient()'><i class='material-icons'>add</i></a></div>")
-     $("#ingredients").append(newInput);
+  var newInput = $("<div><input required type='text' value='' class='ingredients extra-input extra-ing-input' name='ingredients'></input><a class='btn add-btn' onClick='removeInput(this)'><i class='material-icons del-btn-ing'>delete</i></a><a class='btn add-btn' onClick='addIngredient()'><i class='material-icons'>add</i></a></div>");
+  $("#ingredients").append(newInput);
  }
 
  function addStep() {
-     var newInput = $("<div><textarea value='' class='method extra-input extra-step-input' name='method'></textarea><a class='btn add-btn textarea-add-btn' onClick='removeInput(this)'><i class='material-icons del-btn-step'>delete</i></a><a class='btn add-btn textarea-add-btn' onClick='addStep()'><i class='material-icons'>add</i></a></div>")
-     $("#method").append(newInput);
+  var newInput = $("<div><textarea value='' class='method extra-input extra-step-input' name='method'></textarea><a class='btn add-btn textarea-add-btn' onClick='removeInput(this)'><i class='material-icons del-btn-step'>delete</i></a><a class='btn add-btn textarea-add-btn' onClick='addStep()'><i class='material-icons'>add</i></a></div>");
+  $("#method").append(newInput);
  }
 
  // show recipe url preview if not empty
@@ -26,7 +26,7 @@
    document.getElementById('edit-preview').src = '/static/images/default.png';
   }
  }
- 
+
  function showSubmitPreview() {
   var new_url = document.getElementById('recipe_url').value;
   if (document.getElementById('recipe_url').value != "" &&
@@ -42,9 +42,9 @@
  }
 
  $(document).ready(onReady);
- 
- function onReady(){
-    // if recipe_url field is present call showPreview
+
+ function onReady() {
+  // if recipe_url field is present call showPreview
   if (document.getElementById('edit-preview')) {
    showEditPreview();
    document.getElementById('recipe_url').addEventListener('change', showEditPreview);
