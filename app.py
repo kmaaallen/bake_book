d14fd4f1cd0e91eb4942bb05a469cec235a1e290
Kirsten <kmaaallen@gmail.com>
2019-10-26 17:36:10 +0000

    debug false

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ def search_results():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=False)
