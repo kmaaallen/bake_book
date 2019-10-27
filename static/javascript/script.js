bd800feafdcc7d93377c90726d538081a7aed7b0
Kirsten <kmaaallen@gmail.com>
2019-10-27 18:04:05 +0000

    added missing slash

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -59,7 +59,7 @@
 
  // if recipe url invalid
  function invalidUrl(preview){
-    document.getElementById(preview).src = "static/images/default/png";
+    document.getElementById(preview).src = "/static/images/default/png";
     var str = "That URL is invalid. Please submit a valid image URL or leave field blank to use default image";
     document.getElementById("invalid-message").innerHTML = str;
 }
