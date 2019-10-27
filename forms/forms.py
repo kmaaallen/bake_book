f6fec5b77d42ddcc2c96efea5350822c3880a3e1
Kirsten <kmaaallen@gmail.com>
2019-10-27 16:01:55 +0000

    customising invalid message for url

--- forms/forms.py
+++ forms/forms.py
@@ -7,18 +7,17 @@ from wtforms.validators import DataRequired, URL
 class AddRecipeForm(Form):
     recipe_title = StringField("Recipe Title", validators=[DataRequired()])
     sub_title = TextField("Recipe Sub-title", validators=[DataRequired()])
-    recipe_url = URLField("Recipe Image URL", validators=[URL()])
+    recipe_url = URLField("Recipe Image URL", validators=[URL('Please enter a valid url or leave blank to use default image')])
     makes = TextField("Makes", validators=[DataRequired()])
     takes = TextField("Takes", validators=[DataRequired()])
     ingredients = TextField("Ingredients:", validators=[DataRequired()])
     method = TextAreaField("Method:", validators=[DataRequired()])
     submit = SubmitField("Send")
-    
+
 class LoginForm(Form):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    
+
 class SignupForm(Form):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
- 
